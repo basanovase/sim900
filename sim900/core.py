00c08f260d1fd3d76b1bc1168db7fc1f19b26be2
basanovase <flynn.mclean@gmail.com>
2024-05-23 23:57:46 +1200

    Update core.py

--- sim900/core.py
+++ sim900/core.py
@@ -2,14 +2,38 @@ import machine
 import time
 
 class SIM900:
-    def __init__(self, uart_id, baudrate=9600, tx_pin, rx_pin):
+    def __init__(self, uart_id, tx_pin, rx_pin, baudrate=9600):
         self.uart = machine.UART(uart_id, baudrate=baudrate, tx=tx_pin, rx=rx_pin)
 
-    def send_command(self, command, timeout=5000):
-        self.uart.write(command + '\r\n')
+    def send_command(self, command, timeout=3000):
+        command += '\r\n'
+        self.uart.write(command)
         start_time = time.ticks_ms()
-        response = ''
+        response = []
         while time.ticks_diff(time.ticks_ms(), start_time) < timeout:
             if self.uart.any():
-                response += self.uart.read().decode()
-        return response
+                response.append(self.uart.read().decode())
+        return ''.join(response)
+
+    def check_network_registration(self):
+        """
+        Check network registration status.
+        """
+        response = self.send_command('AT+CREG?')
+        if "+CREG: 0,1" in response or "+CREG: 0,5" in response:
+            return True
+        return False
+
+    def check_signal_quality(self):
+        """
+        Check signal quality. Returns the signal strength (0-31, 99).
+        """
+        response = self.send_command('AT+CSQ')
+        csq = response.split(': ')[1].split(',')[0]
+        return int(csq)
+
+    def reset_module(self):
+        """
+        Reset the SIM900 module.
+        """
+        return self.send_command('AT+CFUN=1,1')
